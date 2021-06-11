58534542fedfbdad8eb98b12bb5ebad93b81831e
SeanMiffuine <work.seantang@gmail.com>
2021-06-12 05:38:54 +0800

    fixed typing mechanic

--- gameEngineWUI/gameEngineWUI.cpp
+++ gameEngineWUI/gameEngineWUI.cpp
@@ -20,33 +20,34 @@ int size = sizeof(msg) / sizeof(msg[0]);
 
 int main()
 {
+	HANDLE hDialogue = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hDialogue);
+	DWORD dwBytesWritten = 0;
+
 	while (1)
 	{
-		dialogueLoop();
 		primer();
 		test();
+		WriteConsoleOutputCharacter(hDialogue, dialogueScreen, screenW * screenH, { 0,0 }, &dwBytesWritten);
 	}
 }
 
-void dialogueLoop()
+
+void test()
 {
 	HANDLE hDialogue = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hDialogue);
 	DWORD dwBytesWritten = 0;
 
-	WriteConsoleOutputCharacter(hDialogue, dialogueScreen, screenW * screenH, { 0,0 }, &dwBytesWritten);
-
-}
-
-void test()
-{
 	int size = sizeof(msg) / sizeof(msg[0]); // int of characters in array
 	for (int i = 0; i < size; i++)
 	{
 		dialogueScreen[i] = msg[i];
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		WriteConsoleOutputCharacter(hDialogue, dialogueScreen, screenW * screenH, { 0,0 }, &dwBytesWritten);
 
 	}
+
 }
 
 void primer()
